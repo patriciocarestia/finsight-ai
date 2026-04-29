58956ed4eb88c3ad3264a02b7c7819bff87a0da5
Patricio Carestia <patriciocarestia@gmail.com>
2026-04-28 21:45:55 -0300

    feat: add skeleton screen when graphic is loading

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  private readonly http = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.http.get(`${environment.apiUrl}/health`).subscribe();
+  }
+}
